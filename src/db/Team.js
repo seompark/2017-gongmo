e445a08fd8e17517137743b8625040dd06f0cbc8
a1p4ca <sm@murye.io>
2017-09-12 09:31:54 +0900

    DB wrapper

--- src/db/Team.js
+++ src/db/Team.js
@@ -2,19 +2,50 @@ const knex = require('./knex')
 const team = knex('teams')
 const follower = knex('followers')
 
-/**
- * 팀장 학번으로 팀 정보를 가져온다.
- * @param {string} id
- * @returns {Promise}
- */
-exports.findByLeaderId = async (id) => {
+module.exports = class {
+  /**
+   * @typedef {Object} Team
+   * @property {number} leaderId
+   * @property {Array<Object>} followers
+   * @property {string} formPath
+   * @property {string} serviceDomain
+   *
+   * @param {Team} team
+   */
+  constructor ({name, leaderId, followers, formPath, serviceDomain}) {
+    this.name = name
+    this.leaderId = leaderId
+    this.followers = followers
+    this.formPath = formPath
+    this.serviceDomain = serviceDomain
+  }
 
-}
+  /**
+   * DB에 저장
+   * @returns {Promise}
+   */
+  async save () {
+    await team.insert({
+      name: this.name,
+      leader: this.leaderId,
+      form_path: this.formPath,
+      serviceDomain: this.serviceDomain
+    })
+    for (const value in this.followers) {
+      follower.insert({
+        id: value.id,
+        name: value.name
+      })
+    }
+  }
 
-/**
- * @param {Object} followers
- * @returns {Promise}
- */
-exports.updateFollower = async (followers) => {
-
+  /**
+   * 팀장 학번으로 팀 정보를 가져온다.
+   * @param {string} id
+   * @returns {Promise}
+   */
+  static async findByLeaderId (id) {
+    const followers = await team.slect().where({ leaderId: id })
+    return followers
+  }
 }
