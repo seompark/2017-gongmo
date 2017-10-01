cc9671dac3c69b59983087ab1dcd50c1a8db10b0
a1p4ca <sm@murye.io>
2017-10-01 16:32:58 +0900

    Update ignore_watch

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'gongmo',
       watch: true,
-      ignore_watch: ['node_modules', 'views', 'assets', 'dist', 'contents', '.idea', '.git'],
+      ignore_watch: ['node_modules', 'views', 'assets', 'dist', 'content', '.idea', '.git'],
       script: 'index.js',
       env: {
         NODE_ENV: 'development'
