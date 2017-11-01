f7998f246f40125870f5b9643f91dddb9ff47133
a1p4ca <sm@murye.io>
2017-11-01 23:01:15 +0900

    download ALL implemented

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,11 @@
+const fs = require('fs')
+const path = require('path')
 const Router = require('express').Router
+const archiver = require('archiver')
 const multer = require('multer')
 const router = new Router()
 const Team = require('../src/db/Team')
-const path = require('path')
+const File = require('../src/db/File')
 
 const config = require('../config/index')
 
@@ -26,7 +29,6 @@ router.route('/dashboard')
         user: req.user
       })
     }).catch(_ => {
-      console.log(_)
       res.status(400)
       res.end()
     })
@@ -46,15 +48,50 @@ router.route('/notice')
     })
   })
 
-// API //
-router.route('/u/application')
-  .post(uploadApplication.single('application'), (req, res) => {
+router.get('/download', (req, res) => {
+  const zip = path.join(__dirname, 'content/data/download.zip')
+  const output = fs.createWriteStream(zip)
+  const archive = archiver('zip')
 
+  output.on('close', () => {
+    res.download(zip)
   })
+  archive.pipe(output)
 
-router.route('/u/notice')
+  ;(async () => {
+    const teams = await Team.getList()
+    // 파일이 존재하면
+    for (const team in teams) {
+      if (team.file.formfile || team.file.sourcefile) {
+        // 파일을 가져옴
+        const file = await File.findByLeaderId(team.leader_id)
+        // 소스파일 처리
+        Object.values(file).forEach(file => {
+          if (!file) return
+          archive.file(
+            path.join(config.content, 'files', file.sourcefile.hash),
+            path.join(
+              `${team.name} - ${team.leader_id} ${team.leader_name}`,
+              file.originalName
+            )
+          )
+        })
+      }
+    }
+  })().then(() => {
+    archive.finalize()
+  })
+})
+
+router.route('/notice')
+  .get((req, res) => {
+    // TODO
+    res.render('admin/notice', {
+      notices: []
+    })
+  })
   .post((req, res) => {
-
+    // TODO
   })
 
 module.exports = router
