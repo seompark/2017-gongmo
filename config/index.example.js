6e8253a34229832e89f3aec3a778c762babefd6b
a1p4ca <sm@murye.io>
2017-11-04 21:06:37 +0900

    Now use dotenv

--- config/index.example.js
+++ config/index.example.js
@@ -1,19 +1,20 @@
 const path = require('path')
+require('dotenv').config()
 
 module.exports = {
-  secret: 'I\'m SECRET!!',
+  secret: process.env.COOKIE_SECRET,
   port: 8080,
   dimigo: {
-    host: '',
-    username: 'user',
-    password: 'pwd'
+    host: process.env.DIMIGO_API_HOST,
+    username: process.env.DIMIGO_API_USERNAME,
+    password: process.env.DIMIGO_API_PASSWORD
   },
   db: {
-    host: 'localhost',
-    port: '',
-    username: '',
-    password: '',
-    database: ''
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    username: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
   },
   content: path.resolve(__dirname, '..', 'content')
 }
