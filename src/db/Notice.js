376099087af2dfa2ac2a371493b82ca778407f56
a1p4ca <sm@murye.io>
2017-11-07 17:07:47 +0900

    Add Notice.js

---
+++ src/db/Notice.js
@@ -0,0 +1,27 @@
+const knex = require('knex')
+
+module.exports = class Notice {
+  constructor (message) {
+    this.message = message
+  }
+
+  save () {
+    return knex('notices')
+      .returning('id')
+      .insert({ message: this.message })
+  }
+
+  static async get (id) {
+    const notices = await knex('notices').where({ id }).select()
+    if (notices.length < 1) {
+      return null
+    } else {
+      return notices[0]
+    }
+  }
+
+  static async getList () {
+    const notices = await knex('notices').select()
+    return notices
+  }
+}
